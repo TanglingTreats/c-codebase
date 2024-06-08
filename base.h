6d0870c6f4f0ed042b095d45c1cccaae74a02156
TanglingTreats <foolstacked@gmail.com>
2024-06-08 17:39:27 +0800

    feat: expand context macros.

--- base.h
+++ base.h
@@ -22,4 +22,25 @@
 
 #endif
 
+// NOT DEFINES
+#if !defined(__GNUC__)
+#define COMPILER_GCC 0
+#endif
+
+#if !defined(_M_AMD64)
+#define ARCH_X64 0
+#endif
+
+#if !defined(__aarch64__)
+#define ARCH_ARM64 0
+#endif
+
+#if !defined(__APPLE__) && !defined(__MACH__)
+#define OS_MAC 0
+#endif
+
+#if !defined(__gnu_linux__)
+#define OS_LINUX 0
+#endif
+
 #endif // BASE_H
--- main.c
+++ main.c
@@ -3,8 +3,12 @@
 
 int main() {
   printf("gcc = %d\n", COMPILER_GCC);
+
   printf("macos = %d\n", OS_MAC);
+  printf("linux = %d\n", OS_LINUX);
+
   printf("ARM64 = %d\n", ARCH_ARM64);
+  printf("x86_64 = %d\n", ARCH_X64);
 
   return 0;
 }
